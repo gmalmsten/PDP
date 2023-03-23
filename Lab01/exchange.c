8a621374c31d4233d2e2d8b459e28c340c9e19e6
gmalmsten <gustavmalmsten@gmail.com>
2023-03-23 18:38:31 +0100

    Exercises -2 done

--- Lab01/exchange.c
+++ Lab01/exchange.c
@@ -22,11 +22,24 @@ int main(int argc, char *argv[]) {
   a = 100.0 + (double) rank;  /* Different a on different processors */
 
   /* Exchange variable a, notice the send-recv order */
-
-    MPI_Send(&a, 1, MPI_DOUBLE, next, 69, MPI_COMM_WORLD);
-    MPI_Recv(&b, 1, MPI_DOUBLE, previous, 69, MPI_COMM_WORLD, &status);
-    // MPI_Sendrecv(&a, 1, MPI_DOUBLE, next, 69, &b, 1, MPI_DOUBLE, previous, 69, MPI_COMM_WORLD, &status);
-    printf("Processor %d got %f from processor %d\n", rank, b, previous);
+    MPI_Request request;
+    if(rank == 0){
+      MPI_Isend(&a, 1, MPI_DOUBLE, next, 0, MPI_COMM_WORLD, &request);
+      MPI_Irecv(&b, 1, MPI_DOUBLE, previous, 0, MPI_COMM_WORLD, &request);
+      printf("Processor %d waiting for data\n", rank);
+      MPI_Wait(&request, MPI_STATUS_IGNORE);
+      printf("Processor %d received data\n", rank);
+    }
+    else{
+      
+      MPI_Request request;
+      MPI_Irecv(&b, 1, MPI_DOUBLE, previous, 0, MPI_COMM_WORLD, &request);
+      printf("Processor %d waiting for data\n", rank);
+      MPI_Wait(&request, MPI_STATUS_IGNORE);
+      printf("Processor %d received data, sending to %d\n", rank, next);
+      MPI_Isend(&a, 1, MPI_DOUBLE, next, 0, MPI_COMM_WORLD, &request);
+    }
+    // printf("Processor %d got %f from processor %d\n", rank, b, previous);
   
 
   MPI_Finalize(); 
