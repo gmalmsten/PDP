8057d9b77485a276080d70986c5e25f041836bb3
gmalmsten <gustavmalmsten@gmail.com>
2023-03-24 10:49:51 +0100

    Ran sine on 100000N

--- Ass1/A1/filemaker.c
+++ Ass1/A1/filemaker.c
@@ -3,29 +3,28 @@
 #include<math.h>
 
 #define func(X) sin(X)
+#define PI 3.1415926535897932
 
 int main(int argc, char *argv[]){
 
     if(argc != 5){
-        printf("Usage %s N a b file_ptr\n", argv[0]);
+        printf("Usage %s N n file_ptr\n", argv[0]);
         printf("Where:\nN is the number of datapoints\n");
-        printf("a, b is the function range\n");
+        printf("n is the number of periods\n");
         printf("file_ptr is a pointer to file to write to\n");
     }
 
     const double N = atof(argv[1]);
-    const double a = atof(argv[2]);
-    const double b = atof(argv[3]);
-    char *output_file = argv[4];
+    const double n = atof(argv[2]);
+    char *output_file = argv[3];
 
-    const double dx = (b-a)/(N-1);
+    const double dx = (2*PI)/(N-1);
     printf("dx: %lf\n", dx);
 
     double *x = (double *)malloc(N * sizeof(double));
-    printf("x: ");
     for(int i = 0; i < N; i++){
         x[i] = i*dx;
-        printf("%lf, ", x[i]);
+        
     }
     printf("\n");
     FILE *file;
