dcad43c16a8fd5b83b1ccdb9e54eb3575adf4030
gmalmsten <gustavmalmsten@gmail.com>
2023-05-15 11:43:06 +0200

    Kind of working

--- Project/main.c
+++ Project/main.c
@@ -10,6 +10,7 @@
 #define n 7
 #define T 100
 #define R 15
+#define b 20
 
 void print_d_vec(double *vector, int lim){
     printf("[");
@@ -21,10 +22,14 @@ void print_d_vec(double *vector, int lim){
 
 void print_i_vec(int *vector, int lim){
     printf("[");
-    for(int i = 0; i < lim; i++){
+    for(int i = 0; i < lim - 1; i++){
         printf("%d, ", vector[i]);
     }
-    printf("]\n");
+    printf("%d]\n", vector[lim - 1]);
+}
+
+int cmp (const void *num1, const void *num2) {
+   return ( *(int*)num1 > *(int*)num2 );
 }
 
 int main(int argc, char *argv[]){
@@ -33,21 +38,26 @@ int main(int argc, char *argv[]){
         printf("Usage %s N\n", argv[0]);
         return -1;
     }
+
+    // Arguments / 
     const int N = atoi(argv[1]);
     int rank, num_proc;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);    
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);   
+    const int local_N = N/num_proc;
     
+    // Assumption: N is divisible by num_proc
     if(N%num_proc){
         if(rank == 0){
             printf("ERROR N%%p != 0\n");
             return -2;
         }
     }
-    const int local_N = N/num_proc;
+    
     
     int X[n*local_N];
+    int results[local_N];
     const int P[m*n] = {1, 0, 0, 0, 0, 0, 0,
                         -1, 0, 0, 0, 0, 0, 0,
                         -1, 0, 1, 0, 0, 0, 0,
@@ -65,7 +75,7 @@ int main(int argc, char *argv[]){
                         0, 0, 0, 0, 0, 0, -1};
     double w[15];
     for(int i = 0; i < local_N; i++){
-        srand(time(NULL) + rank*local_N + i);
+        srand(rank*local_N + i);
         double t = 0;
         int x[n] = {900, 900, 30, 330, 50, 270, 20};
         while(t<T){
@@ -103,17 +113,20 @@ int main(int argc, char *argv[]){
             while(sum < lim){   //-----------------------------------------------------------//
                 sum += w[r];
                 r++;
-                if(r>=R){
+                if(r>R){
                     printf("Error: r exceeds the bounds of the w array\n");
                     return -1;
                 }
             }
-            
+
+            r--; // 0 indexing
+
             // Update x
             for(int i = 0; i < n; i++){
                 x[i] += P[r*n + i];
                 if(x[i]<0){
-                    x[i] = 0;
+                    // x[i] = 0;
+                    printf("Warning: x[%d] < 0\n", i);
                 }
             }
 
@@ -123,15 +136,50 @@ int main(int argc, char *argv[]){
         for(int j = 0; j < n; j++){
             X[i*n + j] = x[j];
         }
+        results[i] = x[0];
     }
-        
-   
-    for(int i = 0; i < local_N; i++){
-        printf("Rank %d row %d [", rank, i);
-        for(int j = 0; j < n; j++){
-            printf("%d ", X[i*n + j]);
+
+    // Calculate local and global min and max
+    int global_min, global_max, local_min, local_max;
+
+    local_min = results[0];
+    local_max = results[0];
+    for(int i = 1; i < local_N; i++){
+        if(results[i] < local_min){
+            local_min = results[i];
         }
-        printf("\n");
+        if(results[i] > local_max){
+            local_max = results[i];
+        }
+    }
+
+
+    MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
+
+    // Calculate bin size and calculate the local counts in each bin
+    int bin_size = (global_max - global_min)/b;
+    int bins[b];
+    for(int i = 0; i < b; i++){
+        bins[i] = 0;
+    }
+    qsort(results, local_N, sizeof(int), cmp);
+
+    int bin = 0;    // Current bin
+    for(int i = 0; i < local_N; i++){
+        if(results[i] > global_min + bin_size*(bin+1)){
+            bin++;
+        }
+        bins[bin]++;
+    }
+
+    int global_bins[b];
+
+    MPI_Reduce(bins, global_bins, b, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if(rank == 0)
+    {
+        print_i_vec(global_bins, b);
     }
     MPI_Finalize();
     return 0;
