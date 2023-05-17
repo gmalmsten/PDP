16db51679eda1aeeae4004abbbafaa5aa9a9a7f3
gmalmsten <gustavmalmsten@gmail.com>
2023-05-17 10:50:18 +0200

    Fixed sub timings

--- Project/mc.c
+++ Project/mc.c
@@ -55,7 +55,6 @@ int main(int argc, char *argv[]){
     }
     
     
-    int X[COLS*local_N];
     int results[local_N];
     const int P[ROWS*COLS] = {1, 0, 0, 0, 0, 0, 0,
                             -1, 0, 0, 0, 0, 0, 0,
@@ -79,24 +78,28 @@ int main(int argc, char *argv[]){
     double sub_times[4] = {0, 0, 0, 0};
 
     double start_time = MPI_Wtime();
+    int count = 0;
     for(int epoch = 0; epoch < local_N; epoch++){
         // srand((unsigned int)(rank*local_N + epoch));
-        char timings[3] = {0, 0, 0};
+        char timing = 0;
 
         double t = 0;
         int x[COLS] = {900, 900, 30, 330, 50, 270, 20};
+        double sub_start_time = MPI_Wtime();
         while(t<T){
-            if(!timings[0] && t > T/4){
-                sub_times[0] += (MPI_Wtime() - start_time);
-                timings[0] = 1;
+            if(timing == 0 && t > T/4){
+                sub_times[0] += (MPI_Wtime() - sub_start_time);
+                sub_start_time = MPI_Wtime();
+                timing = 1;
             }
-            if(t > T/2 && !timings[1]){
-                sub_times[1] += (MPI_Wtime() - start_time);
-                timings[1] = 1;
+            if(t > T/2 && timing == 1){
+                sub_times[1] += (MPI_Wtime() - sub_start_time);
+                sub_start_time = MPI_Wtime();
+                timing = 2;
             }
-            if(t > 3*T/4 && !timings[2]){
-                sub_times[2] += (MPI_Wtime() - start_time);
-                timings[2] = 1;
+            if(t > 3*T/4 && timing == 2){
+                sub_times[2] += (MPI_Wtime() - sub_start_time);
+                sub_start_time = MPI_Wtime();
             }
             // Compute w 
             double w[ROWS];
@@ -155,15 +158,14 @@ int main(int argc, char *argv[]){
             // Step time
             t+=tau;
         }
-        sub_times[3] += (MPI_Wtime() - start_time);
-        // for(int j = 0; j < COLS; j++){
-        //     X[epoch*COLS + j] = x[j];
-        // }
+        sub_times[3] += (MPI_Wtime() - sub_start_time);
+
+        // Store result
         results[epoch] = x[0];
     }
 
     for(int i = 0; i < 4; i++){
-        sub_times[i] /= local_N;
+        sub_times[i] /= (double)local_N;
     }
 
     // Calculate local and global min and max
@@ -207,6 +209,7 @@ int main(int argc, char *argv[]){
     double local_time = MPI_Wtime() - start_time;
     double global_time;
     MPI_Reduce(&local_time, &global_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    printf("Rank %d: %lfs %lfs %lfs %lfs\n", rank, sub_times[0], sub_times[1], sub_times[2], sub_times[3]);
     if(rank == 0)
     {
         printf("Time: %lf\n", global_time);
