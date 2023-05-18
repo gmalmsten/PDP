acfe90a350c36955aa110916717d9b4b5eed83dc
gmalmsten <gustavmalmsten@gmail.com>
2023-05-18 13:43:49 +0200

    Optimisations and comments

--- Project/mc.c
+++ Project/mc.c
@@ -11,17 +11,19 @@
 #define T 100
 #define b 20
 
-// #define PRODUCE_OUTPUT
+#define PRODUCE_OUTPUT
 
 void print_d_vec(double *vector, int lim){
+    /*Print a vector consisting of lim doubles*/
     printf("[");
     for(int i = 0; i < lim; i++){
-        printf("%f, ", vector[i]);
+        printf("%lf, ", vector[i]);
     }
     printf("]\n");
 }
 
 void print_i_vec(int *vector, int lim){
+    /*Print a vector consisting of lim integers*/
     printf("[");
     for(int i = 0; i < lim - 1; i++){
         printf("%d, ", vector[i]);
@@ -30,6 +32,7 @@ void print_i_vec(int *vector, int lim){
 }
 
 int cmp (const void *num1, const void *num2) {
+    /*Comparison function for qsort*/
    return ( *(int*)num1 > *(int*)num2 );
 }
 
@@ -40,7 +43,7 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    // Arguments / 
+    // Arguments 
     const int N = atoi(argv[1]);
     int rank, num_proc;
     MPI_Init(&argc, &argv);
@@ -57,7 +60,7 @@ int main(int argc, char *argv[]){
     }
     
     
-    int results[local_N];
+    // Transition matrix
     const int P[ROWS*COLS] = {1, 0, 0, 0, 0, 0, 0,
                             -1, 0, 0, 0, 0, 0, 0,
                             -1, 0, 1, 0, 0, 0, 0,
@@ -75,23 +78,26 @@ int main(int argc, char *argv[]){
                             0, 0, 0, 0, 0, 0, -1};
 
     // Seed
-    time_t seed = time(NULL);
+    // time_t seed = time(NULL);
+    int seed = 1;
     MPI_Bcast(&seed, 1, MPI_INT, 0, MPI_COMM_WORLD);
     srand(seed + rank);
+
     
+    int results[local_N];
+    double sub_times[4] = {0}; 
 
-    double sub_times[4] = {0, 0, 0, 0};
-
+    // Start timer
     double start_time = MPI_Wtime();
-    int count = 0;
     for(int epoch = 0; epoch < local_N; epoch++){
-        // srand((unsigned int)(rank*local_N + epoch));
-        char timing = 0;
-
+        // Initialize new simulation
         double t = 0;
         int x[COLS] = {900, 900, 30, 330, 50, 270, 20};
+
+        char timing = 0;  // Current sub time to store, 0 - 25, 1 - 50, 2 - 75, 3 - 100
         double sub_start_time = MPI_Wtime();
         while(t<T){
+            // Store sub times
             if(timing == 0 && t > T/4){
                 sub_times[0] += (MPI_Wtime() - sub_start_time);
                 sub_start_time = MPI_Wtime();
@@ -107,6 +113,7 @@ int main(int argc, char *argv[]){
                 sub_start_time = MPI_Wtime();
                 timing = 3;
             }
+
             // Compute w 
             double w[ROWS];
             prop(x, w);
@@ -117,28 +124,20 @@ int main(int argc, char *argv[]){
                 a0+=w[i];
             }
             if(a0<0){
-                printf("ERROR a0 < 0\nw: [");
-                for(int i = 0; i < ROWS; i++){
-                    printf("%lf ", w[i]);
-                }
-                printf("]\nx: [");
-                for(int i = 0; i < COLS; i++){
-                    printf("%d ", x[i]);
-                }
-                printf("]\n");
+                printf("ERROR a0 < 0\n");
                 return -3;
             }
 
             // Generate two random numbers
-            double u1 = (double)rand()/RAND_MAX;
-            double u2 = (double)rand()/RAND_MAX;
+            // double u1 = (double)rand()/RAND_MAX;
+            // double u2 = (double)rand()/RAND_MAX;
 
-            double tau = -log(u1)/a0;
+            double tau = -log(((double)rand()/RAND_MAX))/a0;
 
             // Find r
             double sum_r_prev = 0;
             double sum_r = w[0];
-            double lim = a0*u2;
+            double lim = a0*((double)rand()/RAND_MAX);
             int r = 0;
             while(sum_r < lim && sum_r_prev <= lim){   //-----------------------------------------------------------//
                 r++;
@@ -148,28 +147,23 @@ int main(int argc, char *argv[]){
                     printf("Error: r exceeds the bounds of the w array\n");
                     return -1;
                 }
-                
             }
 
 
             // Update x
             for(int i = 0; i < COLS; i++){
                 x[i] += P[r*COLS + i];
-                if(x[i]<0){
-                    x[i] = 0;
-                    printf("Warning: x[%d] < 0\n", i);
-                }
             }
 
             // Step time
             t+=tau;
         }
+        // Store sub time and result
         sub_times[3] += (MPI_Wtime() - sub_start_time);
-
-        // Store result
         results[epoch] = x[0];
     }
 
+    // Rescale sub_times to mean sub_times
     for(int i = 0; i < 4; i++){
         sub_times[i] /= (double)local_N;
     }
@@ -179,26 +173,29 @@ int main(int argc, char *argv[]){
 
     local_min = results[0];
     local_max = results[0];
-    for(int i = 1; i < local_N; i++){
-        if(results[i] < local_min){
-            local_min = results[i];
+    for(int i = 1; i < local_N; i++)
+    {
+        int tmp = results[i];
+        if(tmp < local_min){
+            local_min = tmp;
         }
-        if(results[i] > local_max){
-            local_max = results[i];
+        if(tmp > local_max){
+            local_max = tmp;
         }
     }
 
 
+    // Reduce and broadcast global min and max to all processes
     MPI_Allreduce(&local_min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
     MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 
 
     // Calculate bin size and the local counts in each bin
     int bin_size = (global_max - global_min)/b;
-    int bins[b];
-    for(int i = 0; i < b; i++){
-        bins[i] = 0;
-    }
+    int bins[b] = {0};
+
+
+    // Sort the local results to 
     qsort(results, local_N, sizeof(int), cmp);
 
     int bin = 0;    // Current bin
@@ -206,9 +203,9 @@ int main(int argc, char *argv[]){
         if(results[i] > global_min + bin_size*(bin+1)){
             bin++;
         }
-        if(results[i] > global_min + bin_size*20)
+        if(results[i] > global_min + bin_size*b) // Let last bin contain all elements larger than global_min + 20*bin_size
         {
-            bins[19]++;
+            bins[b-1]++;
         }
         else
         {
@@ -216,25 +213,32 @@ int main(int argc, char *argv[]){
         }
     }
 
+    // Sum all local results in root process (0)
     int global_bins[b];
-
     MPI_Reduce(bins, global_bins, b, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
+    // Gather local sub times in root process
     MPI_Win win;
     MPI_Win_create(&sub_times, 4*sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     MPI_Win_fence(0, win);
-    double all_sub_times[4*num_proc];
-    if(rank == 0)
-    for(int p = 0; p<num_proc; p++)
-    {
-        MPI_Get(&all_sub_times[4*p], 4, MPI_DOUBLE, p, 0, 4, MPI_DOUBLE, win);
-    }
+        double all_sub_times[4*num_proc];
+        if(rank == 0)
+            for(int p = 0; p<num_proc; p++)
+            {
+                MPI_Get(&all_sub_times[4*p], 4, MPI_DOUBLE, p, 0, 4, MPI_DOUBLE, win);
+            }
     MPI_Win_fence(0, win);
     MPI_Win_free(&win);
+    
+
+    // Stop timer
     double local_time = MPI_Wtime() - start_time;
     double global_time;
     MPI_Reduce(&local_time, &global_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
     
 
+    // Produce output
     if(rank == 0)
     {
         #ifdef PRODUCE_OUTPUT
